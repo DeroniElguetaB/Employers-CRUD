79464f5b4d49d4ce7a948953125d6b98aa752316
Deroni Elgueta <deroniaeb@gmail.com>
2024-04-01 22:53:32 -0300

    inicializacion employers modificaciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  const [nombre, setNombre] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='datos'>
+        <label>Nombre: <input type='text'></input></label>
+        <label>Edad: <input type='text'></input></label>
+        <label>Pais: <input type='text'></input></label>
+        <label>Cargo: <input type='text'></input></label>
+        <label>Año: <input type='number'></input></label>
+        <button>Registrar</button>
+      </div>
     </div>
   );
 }
