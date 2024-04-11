2d35b2a1f82a8c5eafe2b85b89cfd43e1b6edaca
Deroni Elgueta <deroniaeb@gmail.com>
2024-04-11 19:22:46 -0400

    CRUD adelanto

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { useState } from 'react'
 
 function App() {
 
-  const [nombre, setNombre] = useState()
+  const [nombre, setNombre] = useState('')
+  const [edad, setEdad] = useState(0)
+  const [pais, setPais] = useState('')
+  const [cargo, setCargo] = useState('')
+  const [anios, setAnios] = useState(0)
 
   return (
     <div className="App">
       <div className='datos'>
-        <label>Nombre: <input type='text'></input></label>
-        <label>Edad: <input type='text'></input></label>
-        <label>Pais: <input type='text'></input></label>
-        <label>Cargo: <input type='text'></input></label>
-        <label>Año: <input type='number'></input></label>
-        <button>Registrar</button>
+        <label>Nombre: <input onChange={(event) => {setNombre(event.target.value)}} type='text'></input></label>
+        <label>Edad: <input onChange={(event) => {setEdad(event.target.value)}} type='number'></input></label>
+        <label>Pais: <input onChange={(event) => {setPais(event.target.value)}} type='text'></input></label>
+        <label>Cargo: <input onChange={(event) => {setCargo(event.target.value)}} type='text'></input></label>
+        <label>Año: <input onChange={(event) => {setAnios(event.target.value)}} type='number'></input></label>
+        <button onClick={mostrarDatos}>Registrar</button>
       </div>
     </div>
   );
